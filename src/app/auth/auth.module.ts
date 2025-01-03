3f02d4b82aee4ffff681d41df8f043ccc8eb0a32
Ikadev95 <fegughi@gmail.com>
2025-01-03 18:37:47 +0100

    work in progress login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
-    AuthComponent
+    AuthComponent,
+    RegisterComponent,
   ],
   imports: [
     CommonModule,
